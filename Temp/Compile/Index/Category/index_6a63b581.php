7b08607324871b2e5ee3826129bf19fd3e31a2a4
houbei@aliyun.com <houbei@aliyun.com>
2015-02-17 23:25:29 +0800

    修复页脚信息错误

--- Temp/Compile/Index/Category/index_6a63b581.php
+++ Temp/Compile/Index/Category/index_6a63b581.php
@@ -416,7 +416,7 @@
                 <a href="#" target="_blank">用户中心</a>|
                 <a href="http://127.0.0.1/houbeicms/index.php?g=Addon&m=Sitemap&c=Index&a=index">网站地图</a>
                 <br>
-                © 2012 - 2014 hdphp.com. All Rights Reserved (京ICP备12048441号-1)
+                Copyright &copy 2015  www.houbei.cc, All Rights Reserved 后备网 版权所有 黔ICP备15000001号
             </div>
         </div>
     </div>
